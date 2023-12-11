721ee04ee53525625c8bd711ff6cf496f987b4c1
Dylan-Mutabazi <152308287+Dylan-Mutabazi@users.noreply.github.com>
2023-12-11 11:41:30 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Silly)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+})
+basic.forever(function () {
+	
+})
